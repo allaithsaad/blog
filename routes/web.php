da811096e7cf17f0beffb5f6f115a962970385b1
Al-laith Saad Al-aqra'a <75394655+allaithsaad@users.noreply.github.com>
2024-03-17 04:36:48 +0300

    complete the tasks app

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Requests\TaskRequest;
+use App\Models\Task;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,17 +17,60 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
+
+
 Route::get('/', function () {
-    return " { ' hello ' : ' world ' } ";
+    return redirect()->route('tasks.index');
 });
 
-Route::get('/hello', function () {
-    return 'dddd';
-})->name('hello');
-Route::get('/hallo', function () {
-    return redirect()->route('hello');
-});
+Route::get('/tasks', function () {
+    return view('index', [
+        'tasks' => Task::latest()->paginate(10)
+    ]);
+})->name('tasks.index');
 
-Route::get('/greet/{name}', function ($name) {
-    return 'Hello, '. $name.' !';
-});
+Route::view('/tasks/create', 'create')->name('tasks.create');
+
+Route::get('/tasks/{task}/edit', function (Task $task) {
+    return view('edit', ['task' => $task]);
+})->name('tasks.edit');
+
+Route::put('/tasks/{task}/toggle-completed', function (Task $task) {
+    $task->toggleComplete();
+    return redirect()->back()->with('success', 'Task update Successfully');
+})->name('tasks.toggle-completed');
+
+Route::put('/tasks/{task}', function (Task $task, TaskRequest $req) {
+    $task->update($req->validated());
+    return redirect()->route('tasks.show', ['task' => $task->id])->with('success', 'Task Edit Successfully');
+})->name('tasks.update');
+
+Route::get('/tasks/{task}', function (Task $task) {
+    return view('show', ['task' => $task]);
+})->name('tasks.show');
+
+
+
+
+Route::post('/tasks', function (TaskRequest $req) {
+
+    $task = Task::create($req->validated());
+    return redirect()->route('tasks.show', ['task' => $task->id])->with('success', 'Task Created Successfully');
+})->name('tasks.store');
+
+
+
+Route::delete('/tasks/{task}', function (Task $task) {
+    $task->delete();
+    return redirect()->route('tasks.index')->with('success', 'Task Deleted Successfully');
+})->name('tasks.destroy');
+
+
+
+Route::fallback(
+    function () {
+        return 'Nothing to find here Dear';
+    }
+);
